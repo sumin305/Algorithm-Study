554ee797d581e4cbd342c863f53d2270cde0c3c8
이수민 <gogotnals@naver.com>
2023-11-09 23:03:34 +0900

    [BOJ] 10942 팰린드롬?

---
+++ BOJ/Gold/10942_팰린드롬/10942.py
@@ -0,0 +1,25 @@
+## 10942
+import sys
+input = sys.stdin.readline
+N = int(input())
+arr = list(map(int, input().split()))
+
+dp = [[0 for i in range(N)] for j in range(N)]
+
+for i in range(N):
+    dp[i][i] = 1
+    if i < N-1 and arr[i] == arr[i+1]:
+        dp[i][i+1] = 1
+
+# dp 초기화
+for j in range(N):
+    for i in range(N):
+        if j < i or i == j: continue # 항상 S는 E보다 작으므로
+        if arr[i] == arr[j] and dp[i+1][j-1]:
+            dp[i][j] = 1
+            
+M = int(input())
+
+for _ in range(M):
+    S, E = map(int, input().split())
+    print(dp[S-1][E-1])
